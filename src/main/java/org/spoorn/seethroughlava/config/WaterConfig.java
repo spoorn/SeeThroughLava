755a396edbdf948e00e5d44a9d6f95086daf832d
spoorn <spookump@gmail.com>
2022-01-16 03:18:09 -0800

    Bump to 1.3
    Change water config to be default to true

--- src/main/java/org/spoorn/seethroughlava/config/WaterConfig.java
+++ src/main/java/org/spoorn/seethroughlava/config/WaterConfig.java
@@ -5,8 +5,8 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 public class WaterConfig {
 
     @Comment("True if this mod should override the native fog density of water with below configured value [default = false]")
-    public boolean shouldOverrideWaterFogDensity = false;
+    public boolean shouldOverrideWaterFogDensity = true;
 
     @Comment("Fog density for water [0.0 to 1.0; 0 means no fog, 1 is full fog density] [default = 0.01] [vanilla = 0.05]")
-    public double waterFogDensity = 0.01;
+    public double waterFogDensity = 0.03;
 }
