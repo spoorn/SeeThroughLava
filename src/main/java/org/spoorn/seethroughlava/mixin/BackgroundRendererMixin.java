4d1bb515d113ff793ece468a16d29521d7ea372c
spoorn <spookump@gmail.com>
2023-07-04 21:11:04 -0700

    Bump to 4.0-1.20
    Update to Minecraft 1.20+

--- src/main/java/org/spoorn/seethroughlava/mixin/BackgroundRendererMixin.java
+++ src/main/java/org/spoorn/seethroughlava/mixin/BackgroundRendererMixin.java
@@ -1,14 +1,12 @@
 package org.spoorn.seethroughlava.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.CameraSubmersionType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spoorn.seethroughlava.config.LavaConfig;
 import org.spoorn.seethroughlava.config.ModConfig;
